c6cfb05567e495c0eeee5a6ee99b9205758f9865
hasandemirr <hasan.demir.0396@gmail.com>
2022-06-16 13:33:22 +0300

    Iqerable Ienumararble

---
+++ Iquerable Ienumarable/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iquerable_Ienumarable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> integerList = new List<int>()
+            {
+                1,2,3,4,5,6,7,8,9,10
+            };
+
+            //IEnumerable<int> QuerySyntax = from obj in integerList where obj > 5 select obj;
+            IQueryable<int> QuerySyntax = from obj in integerList.AsQueryable() where obj > 5 select obj;
+
+            //var QuerySyntax =from obj 
+            //                 in integerList 
+            //                 where obj>5 
+            //                 select obj;
+
+            foreach (var item in QuerySyntax)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
